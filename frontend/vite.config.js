b6befffeae522fb75ca5ebc336d2a9698eebb24d
SmallCakekoo <payan.sary@gmail.com>
2025-09-15 23:19:54 -0500

    chore: initialize project scaffolding

---
+++ frontend/vite.config.js
@@ -0,0 +1,19 @@
+import { defineConfig } from "vite";
+import path from "path";
+
+export default defineConfig({
+  root: "frontend/public", // apunta a la carpeta public
+  server: {
+    port: 5000, // puerto de desarrollo
+    open: true,
+  },
+  build: {
+    outDir: "../../dist", // build final fuera de frontend
+    rollupOptions: {
+      input: {
+        client: path.resolve(__dirname, "public/client/index.html"),
+        admin: path.resolve(__dirname, "public/admin/index.html"),
+      },
+    },
+  },
+});
