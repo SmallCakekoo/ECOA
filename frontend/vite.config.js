4a7a040f13410fdf83e15dc54b955a2e6d94821e
Ana Tobon <anita.tobon28@gmail.com>
2025-10-27 05:50:48 -0500

    :)

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,18 +1,22 @@
 import { defineConfig } from "vite";
 import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export default defineConfig({
-  root: "../frontend/public", // apunta a la carpeta public
+  root: "./public", // apunta a la carpeta public
   server: {
-    port: 5000, // puerto de desarrollo
+    port: 5000,
     open: true,
   },
   build: {
-    outDir: "../../dist", // build final fuera de frontend
+    outDir: "../dist", // build final en frontend/dist
     rollupOptions: {
       input: {
-        client: path.resolve(__dirname, "public/client/index.html"),
-        admin: path.resolve(__dirname, "public/admin/index.html"),
+        client: path.resolve(__dirname, "public/client/screens/Home/index.html"),
+        admin: path.resolve(__dirname, "public/admin/screens/dashboard/index.html"),
       },
     },
   },
